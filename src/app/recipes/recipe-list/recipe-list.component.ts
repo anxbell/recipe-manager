e95632fbbae7abaa21d5f6833679021a3c202c92
anxbell <valerieanabell@gmail.com>
2025-05-20 16:19:23 -0600

    Passing Data recipies, with event and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output} from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model'
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe
       (
@@ -17,9 +18,13 @@ export class RecipeListComponent {
       ),
     new Recipe
       (
-        'A test recipe 2',
+        'Another test recipe',
         'This is a test',
         'https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'
       )
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
